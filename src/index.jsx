7f2935d4b2495d8a9ebaedca9baa2f419ec15b47
Inna Batalova <batalovainna97@gmail.com>
2023-10-17 17:56:53 +0300

    Init: setting up dependencies

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+import LayoutBlock from './LayoutBlock';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <LayoutBlock />
+);
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  },
+  devtool: "source-map",
+  stats: {
+    children: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "postcss-loader", "resolve-url-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: 'asset/resource',
+      },
+      { test: /\.(js|jsx)?$/, use: "babel-loader" }
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/static/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src", to: "dist" }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].scss",
+    }),
+  ],
+};
